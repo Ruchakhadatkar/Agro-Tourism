edc2d7ffe6b89e26fbe77edb6020a51782e924fe
Rucha Khadatkar <ruchakhadatkar83@gmail.com>
2024-08-13 15:54:25 +0530

    bugs fixed

--- src/pages/ProductsPage/ProductPage.js
+++ src/pages/ProductsPage/ProductPage.js
@@ -1,46 +1,50 @@
 import React from "react";
 import "./ProductPage.css";
-import overview from "../../Assets/overview/Plan-overview.jpg";
-
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
-import seedPlant from "../../Assets/Explore/exploreImg.jpg";
-import organic from "../../Assets/Explore/explore-img-2.jpg";
-import nursery from "../../Assets/Explore/plant-nursery.jpg";
-import cattle from "../../Assets/Explore/explore-img-4.jpg";
+import vegetableFarm from "../../Assets/ProductPage/vegetable-farm.jpg";
+import dairy from "../../Assets/ProductPage/Dairy-Farming.jpg";
+import flowerFarm from "../../Assets/ProductPage/flower-farming-pic.jpg";
 
 const ProductPage = () => {
   return (
     <div className="container-products">
-      <div className="card-container d-flex">
+      <div className="heading">
+        <h5>Naturally Grown, Perfectly Fresh </h5>
+        <h2>Our Products</h2>
+      </div>
+      <div className="card-container d-flex ">
         <div className="card-1">
-          <div class="card" style={{ width: "18rem" }}>
-            <img src={nursery} class="card-img-top" alt="" />
+          <div class="card" style={{ width: "22rem" }}>
+            <img src={vegetableFarm} class="card-img-top" alt="" />
             <div class="card-body">
+              <h2>Vegetable Farming</h2>
               <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Sustainably grown, organic vegetables, cultivated with care and
+                tradition.
               </p>
             </div>
           </div>
         </div>
         <div className="card-2">
-          <div class="card" style={{ width: "18rem" }}>
-            <img src={organic} class="card-img-top" alt="" />
+          <div class="card" style={{ width: "22rem" }}>
+            <img src={dairy} class="card-img-top" alt="" />
             <div class="card-body">
+              <h2>Dairy products</h2>
               <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Naturally crafted dairy, delivering freshness and quality from
+                our farm.
               </p>
             </div>
           </div>
         </div>
         <div className="card-3">
-          <div class="card" style={{ width: "18rem" }}>
-            <img src={cattle} class="card-img-top" alt="" />
+          <div class="card" style={{ width: "22rem" }}>
+            <img src={flowerFarm} class="card-img-top" alt="" />
             <div class="card-body">
+              <h2>Flower Farming</h2>
+
               <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Cultivating vibrant blooms, our flower farming brings nature's
+                beauty to life.
               </p>
             </div>
           </div>
