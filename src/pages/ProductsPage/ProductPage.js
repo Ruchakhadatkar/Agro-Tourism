548203b7ca344e323dcd54e376bf1cc427ade18c
Rucha Khadatkar <ruchakhadatkar83@gmail.com>
2024-08-13 17:29:12 +0530

    bugs fixed

--- src/pages/ProductsPage/ProductPage.js
+++ src/pages/ProductsPage/ProductPage.js
@@ -3,6 +3,11 @@ import "./ProductPage.css";
 import vegetableFarm from "../../Assets/ProductPage/vegetable-farm.jpg";
 import dairy from "../../Assets/ProductPage/Dairy-Farming.jpg";
 import flowerFarm from "../../Assets/ProductPage/flower-farming-pic.jpg";
+import { GiPlantSeed } from "react-icons/gi";
+import { FaTractor } from "react-icons/fa";
+
+import { GiFruitBowl } from "react-icons/gi";
+import { GiWateringCan } from "react-icons/gi";
 
 const ProductPage = () => {
   return (
@@ -50,7 +55,62 @@ const ProductPage = () => {
           </div>
         </div>
       </div>
-      <div className="info-container"></div>
+      <div className="info-container">
+        <div className="heading">
+          <h5>Our Benefits </h5>
+          <h2 className="head">Agriculture & Eco Farming</h2>
+          <p className="para">
+            This form of tourism helps one stay with the farmer, engage oneself
+            in farming activities, and eat authentic food. It involves staying
+            in a farmhouse or separate guest house which will provide meals,
+            organize activities involving observing and participating in farming
+            operations. It is a leisure activity in an agricultural environment
+            with an opportunity to help farming while staying.
+          </p>
+        </div>
+        <div className="card-icon d-flex">
+          <div class="card cards">
+            <div class="card-body">
+              <div className="icon">
+                <i>
+                  <FaTractor />
+                </i>
+              </div>
+              <h2 className="card-head">Old & New Technology</h2>
+            </div>
+          </div>
+          <div class="card cards" >
+            <div class="card-body">
+              <div className="icon">
+                <i>
+                  <GiPlantSeed />
+                </i>
+              </div>
+              <h2 className="card-head">Vegetable Farms</h2>
+            </div>
+          </div>
+          <div class="card cards" >
+            <div class="card-body">
+              <div className="icon">
+                <i>
+                  <GiWateringCan />
+                </i>
+              </div>
+              <h2 className="card-head">Watering Plants</h2>
+            </div>
+          </div>
+          <div class="card cards" >
+            <div class="card-body">
+              <div className="icon">
+                <i>
+                  <GiFruitBowl />
+                </i>
+              </div>
+              <h2 className="card-head">Qaulity Products</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
